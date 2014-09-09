cb17e0c9d4ba585c67e79bccda87181b8955f68c
Simon Ross Moller <srmo057@cse.unsw.EDU.AU>
2014-09-09 15:55:38 +1000

    Initial Commit

---
+++ SummaryMarketDataService/src/au/edu/unsw/services/SummaryMarketDataServiceSkeleton.java
@@ -0,0 +1,33 @@
+
+/**
+ * SummaryMarketDataServiceSkeleton.java
+ *
+ * This file was auto-generated from WSDL
+ * by the Apache Axis2 version: 1.6.2  Built on : Apr 17, 2012 (05:33:49 IST)
+ */
+    package au.edu.unsw.services;
+    /**
+     *  SummaryMarketDataServiceSkeleton java skeleton for the axisService
+     */
+    public class SummaryMarketDataServiceSkeleton implements SummaryMarketDataServiceSkeletonInterface{
+        
+         
+        /**
+         * Auto generated method signature
+         * 
+                                     * @param summaryMarketData0 
+             * @return summaryMarketDataResponse1 
+             * @throws SummaryMarketDataFaultException 
+         */
+        
+                 public au.edu.unsw.sltf.services.SummaryMarketDataResponseDocument summaryMarketData
+                  (
+                  au.edu.unsw.sltf.services.SummaryMarketDataDocument summaryMarketData0
+                  )
+            throws SummaryMarketDataFaultException{
+                //TODO : fill this with the necessary business logic
+                throw new  java.lang.UnsupportedOperationException("Please implement " + this.getClass().getName() + "#summaryMarketData");
+        }
+     
+    }
+    
